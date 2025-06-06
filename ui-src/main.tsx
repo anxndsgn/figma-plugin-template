17215b5eff8e1e0f7fc01449c278efe92c4c4b9b
XIN <anxanxx@gmail.com>
2025-06-06 13:32:16 +0800

    refactor(main): update ReactDOM rendering to use createRoot and StrictMode

--- ui-src/main.tsx
+++ ui-src/main.tsx
@@ -1,4 +1,9 @@
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
