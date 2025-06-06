df06930363a41bbfcd732b907f2f742673fd2f78
XIN <anxanxx@gmail.com>
2025-06-06 15:39:07 +0800

    feat: add shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { viteSingleFile } from "vite-plugin-singlefile";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root: "./ui-src",
   plugins: [react(), viteSingleFile()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./ui-src"),
+    },
+  },
   build: {
     target: "esnext",
     assetsInlineLimit: 100000000,
